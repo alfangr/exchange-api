098b94159cfeb77489a14af2e719633dfcf32e12
Alfan Ghinan Rusydi <alfangr05gmail.com@Alfans-MacBook-Pro.local>
2023-07-01 14:55:25 +0700

    [fix] debug error

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/exchange', async (req, res) => {
     const convertResult = await convertCurrency(from, to, amount);
     res.json(convertResult);
   } catch (error) {
-    console.error(error);
+    console.log(error);
     res.status(500).json({ message: 'An error occurred while converting the currency' });
   }
 });
